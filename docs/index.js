f7002d656b6d48ab593d4a07d561a91f2607044a
yuche <i@yuche.me>
2015-10-01 23:31:27 +0800

    adjust aslant

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,12 @@
 import './assets/style.css'
 import 'prismjs'
-import 'prismjs/plugins/file-highlight/prism-file-highlight.js'
 
 import affix from '../src/Affix.vue'
 import container from './example/container.vue'
 import headerDocs from './example/headerDocs.vue'
 import alertDocs from './example/alertDocs.vue'
 
-const docs = new Vue({
+new Vue({
   el: '#wrapper',
   components: {
     affix,
