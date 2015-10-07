be2fdfff5b16ccee234c8bbfdd2c0232f2059b99
yuche <i@yuche.me>
2015-10-07 09:57:05 +0800

    roll

---
+++ REDEME.md
@@ -0,0 +1,9 @@
+# vue-strap
+Bootstrap components built with Vue.js
+
+## Docs
+
+## Local Setup
+* Install with `npm install`
+* Run the docs site in development mode with `npm run dev`
+* Build with `npm run build`
--- src/Panel.vue
+++ src/Panel.vue
@@ -9,8 +9,10 @@
       </h4>
     </div>
     <div class="panel-collapse"
+      v-el="panel"
       v-show="isOpen"
-      v-transition="collapse">
+      v-transition="collapse"
+    >
       <div class="panel-body">
         <content></content>
       </div>
@@ -29,11 +31,23 @@
         type: String
       }
     },
+    data() {
+      return {
+        height: 0
+      }
+    },
     methods: {
       toggleIsOpen() {
         this.isOpen = !this.isOpen
         this.$dispatch('isOpenEvent', this)
       }
+    },
+    ready() {
+      const panel = this.$$.panel
+      panel.style.display = 'block'
+      this.height = panel.offsetHeight
+      panel.style.maxHeight = this.height + 'px'
+      if (!this.isOpen) panel.style.display = 'none'
     }
   }
 </script>
@@ -42,12 +56,14 @@
 .accordion-toggle {
   cursor: pointer;
 }
+
 .collapse-transition {
 transition: max-height .5s ease;
-max-height: 150px;
 overflow: hidden;
 }
+
 .collapse-enter, .collapse-leave {
-  max-height: 0;
+  max-height: 0!important;
 }
+
 </style>
