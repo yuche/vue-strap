f96fc12675ee9ac00370f9e37b0d5c0e2a143998
Peter Coles <peter@kuroiweb.com>
2016-02-18 17:20:38 +0000

    Remove debug statement

--- src/Typeahead.vue
+++ src/Typeahead.vue
@@ -128,7 +128,6 @@ const typeahead = {
         return this.current === index
       },
       hit(e) {
-        console.log("e", e, "e.targetVm", e.targetVM);
         e.preventDefault()
         this.onHit(this.items[this.current], this);
       },
