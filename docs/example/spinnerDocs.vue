89511dab4554f89c62f685c253629b8ea0649caa
Aman Patel <aman.patel@stjude.org>
2016-05-13 13:37:38 -0500

    Documentation for spinner component.
    This reverts commit 2c30dde2b5ac9fbd0d738c74e5c2ff534248c08e.

--- docs/example/spinnerDocs.vue
+++ docs/example/spinnerDocs.vue
@@ -24,11 +24,44 @@
       <!-- Html markup end-->
 
     </div>
+
+    <pre><code class="language-markup"><script type="language-mark-up">
+		<spinner id="spinner-box" :size="(sm,md,lg...)" :fixed="(true,false)" 
+			text="I will close in 2 secs" v-ref:spinner></spinner>
+	</script></code></pre>
+
+	<p>To close the spinner, use the following code:</p>
+	<pre><code class="language-markup"><script type="language-mark-up">this.$refs.spinner.hide()</script></code></pre>
+  <h2>Spinner Options</h2>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Default</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>size</td>
+        <td><code>Text</code></td>
+        <td>md</td>
+        <td>The size of the spinner (either [sm, md, lg]).</td>
+      </tr>
+      <tr>
+        <td>fixed</td>
+        <td><code>Boolean</code></td>
+        <td>false</td>
+        <td>Set to true if you want the spinner to occupy the entire window space.</td>
+      </tr>
+    </tbody>
+  </table>
   </div>
 </template>
 
 <script>
-  import spinner from 'src/Spinner.vue'
+  import spinner from 'src/Spinner.vue';
 
   const sizes = [
     {
@@ -58,7 +91,7 @@
       'shown::spinner'(id) {
         setTimeout(() => {
           this.$root.$broadcast('hide::spinner', id)
-      }, 2000)
+	    }, 2000)
       }
     }
   }
