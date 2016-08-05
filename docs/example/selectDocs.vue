89a5b7802e87c8cd589b793e72cfc652722954b4
Willem <willemfranco@gmail.com>
2016-08-05 15:30:30 -0400

    added input native validation. cleaning and ordering select
    - run builds

--- docs/example/selectDocs.vue
+++ docs/example/selectDocs.vue
@@ -21,7 +21,7 @@
       <form action="./#select" method="get">
         <v-select :options="select.options" :value.sync="select.value" :name="select.inputs?(select.multiple?'animals[]':'animal'):''"
           :multiple="select.multiple" :search="select.search" :justified="select.justified" :required="select.inputs&&select.required"
-          :clear-button="select.resetButton" :close-on-select="select.closeOnSelect" :limit="select.limit?3:1024" :disabled="select.disabled"
+          :clear-button="select.clearButton" :close-on-select="select.closeOnSelect" :limit="select.limit?3:1024" :disabled="select.disabled"
         ></v-select>
         <button v-if="select.inputs" type="submit" class="btn btn-default">Submit form</button>
       </form>
@@ -33,14 +33,13 @@
         <label v-if="select.multiple"><input type="checkbox" v-model="select.closeOnSelect"/> Close on Select</label>
       </div>
       <div class="checkbox"><label><input type="checkbox" v-model="select.justified"/> Justified</label></div>
-      <div class="checkbox"><label><input type="checkbox" v-model="select.resetButton"/> Show Reset Button</label></div>
+      <div class="checkbox"><label><input type="checkbox" v-model="select.clearButton"/> Clear Button</label></div>
       <div class="checkbox">
         <label><input type="checkbox" v-model="select.inputs"/> Form input</label>
         <label v-if="select.inputs"><input type="checkbox" v-model="select.required"/> Required (add empty value if noting selected)</label>
       </div>
     </div>
     <pre><code class="language-markup"><script type="language-mark-up">
-Simple:
 <v-select>
   <v-option value="apple">Apple</v-option>
   <v-option value="banana">Banana</v-option>
@@ -49,52 +48,41 @@ Simple:
   <v-option value="grape">Grape</v-option>
 </v-select>
 
-Test options:
 <form action="./#select" method="get">
-  <v-select :options="select.options" :value.sync="select.value" :name="select.inputs?(select.multiple?'animals[]':'animal'):''"
-    :multiple="select.multiple" :search="select.search" :justified="select.justified" :required="select.inputs&&select.required"
-    :clear-button="select.resetButton" :close-on-select="select.closeOnSelect" :limit="select.limit?3:1024" :disabled="select.disabled"></v-select>
-  <button v-if="select.inputs" type="submit" class="btn btn-default">Submit form</button>
+  <v-select :value.sync="select.value" :options="select.options"
+    multiple name="animals[]" limit="3"
+    search justified required disabled
+    clear-button close-on-select
+  ></v-select>
+  <button type="submit" class="btn btn-default">Submit form</button>
 </form>
-<div class="checkbox"><label><input type="checkbox" v-model="select.disabled"/> Disabled</label></div>
-<div class="checkbox"><label><input type="checkbox" v-model="select.search"/> Search</label></div>
-<div class="checkbox">
-  <label><input type="checkbox" v-model="select.multiple"/> Multiple</label>
-  <label v-if="select.multiple"><input type="checkbox" v-model="select.limit"/> Limit (e.g. 3)</label>
-  <label v-if="select.multiple"><input type="checkbox" v-model="select.closeOnSelect"/> Close on Select</label>
-</div>
-<div class="checkbox"><label><input type="checkbox" v-model="select.justified"/> Justified</label></div>
-<div class="checkbox"><label><input type="checkbox" v-model="select.resetButton"/> Show Reset Button</label></div>
-<div class="checkbox">
-  <label><input type="checkbox" v-model="select.inputs"/> Form input</label>
-  <label v-if="select.inputs"><input type="checkbox" v-model="select.required"/> Required (add empty value if noting selected)</label>
-</div>
+    </script></code></pre>
+    <pre><code class="language-javascript"><script type="language-javascript">
 options: [
-  {value:1, label:'Cat'},
-  {value:2, label:'Cow'},
-  {value:3, label:'Dog'},
-  {value:4, label:'Elephant'},
-  {value:5, label:'Fish'},
-  {value:6, label:'Lion'},
-  {value:7, label:'Tiger'},
-  {value:8, label:'Turtle'}
+  {value: 1, label: 'Cat'},
+  {value: 2, label: 'Cow'},
+  {value: 3, label: 'Dog'},
+  {value: 4, label: 'Elephant'},
+  {value: 5, label: 'Fish'},
+  {value: 6, label: 'Lion'},
+  {value: 7, label: 'Tiger'},
+  {value: 8, label: 'Turtle'}
 ]
     </script></code></pre>
 
       <hr />
-      <h4>Ajax data & parent dependency:</h4>
+      <h4>Ajax data and parent dependency:</h4>
       <p>
-        The second element has inheritance. Enable when the first get some value & the ajax return values.
+        The second element has inheritance. Enable when the first get some value and the ajax return values.
       </p>
       <v-select url="docs/data.json" :value.sync="ajax.value" clear-button></v-select>
       <v-select url="docs/data.json" multiple :parent="ajax.value"></v-select>
     <pre><code class="language-markup">
-Ajax:
 &lt;v-select url="docs/data.json" :value.sync="ajax.value" clear-button>&lt;/v-select>
 &lt;v-select url="docs/data.json" multiple :parent="ajax.value">&lt;/v-select>
     </code></pre>
 
-    <h2>Select Options</h2>
+    <h2>Other Options</h2>
     <table class="table table-bordered">
       <thead>
         <tr>
@@ -106,10 +94,16 @@ Ajax:
       </thead>
       <tbody>
         <tr>
-          <td>value</td>
-          <td><code>Array</code></td>
-          <td><code>[]</code></td>
+          <td>close-on-select</td>
+          <td><code>Boolean</code></td>
+          <td><code>false</code></td>
           <td></td>
+        </tr>
+        <tr>
+          <td>lang</td>
+          <td><code>String</code></td>
+          <td>Browser language</td>
+          <td><abbr title="ISO 639-1 code"><a href="https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes">Language</a></abbr>. Default <code>en</code> if the translation doesn't exist.</td>
         </tr>
         <tr>
           <td>placeholder</td>
@@ -118,21 +112,9 @@ Ajax:
           <td></td>
         </tr>
         <tr>
-          <td>multiple</td>
-          <td><code>Boolean</code></td>
-          <td><code>false</code></td>
+          <td>search-text</td>
+          <td><code>String</code></td>
           <td></td>
-        </tr>
-        <tr>
-          <td>limit</td>
-          <td><code>Number</code></td>
-          <td><code>1024</code></td>
-          <td>Limit the number of elements you are allowed to select.</td>
-        </tr>
-        <tr>
-          <td>disabled</td>
-          <td><code>Boolean</code></td>
-          <td><code>false</code></td>
           <td></td>
         </tr>
       </tbody>
