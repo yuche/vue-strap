133de6feddcb284cecd4aebebde01615ead5073c
Willem <willemfranco@gmail.com>
2016-08-20 22:01:09 -0400

    Affix now work more like the original bootstrap

--- src/Affix.vue
+++ src/Affix.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
-    <div v-bind:class="{'vue-affix': affixed}"
-      v-bind:style="styles">
+  <div class="hidden-print hidden-xs hidden-sm">
+    <nav class="bs-docs-sidebar" :class="{affix:affixed}" :style="{marginTop:top}">
       <slot></slot>
-    </div>
+    </nav>
   </div>
 </template>
 
@@ -21,66 +20,49 @@ export default {
   },
   data () {
     return {
-      affixed: false,
-      styles: {}
+      affixed:false
+    }
+  },
+  computed: {
+    top () {
+      return this.offset > 0 ? this.offset + 'px' : null
     }
   },
   methods: {
-    scrolling () {
-      const scrollTop = this.getScroll(window, true)
-      const elementOffset = this.getOffset(this.$el)
-      if (!this.affixed && scrollTop > elementOffset.top) {
-        this.affixed = true
-        this.styles = {
-          top: this.offset + 'px',
-          left: elementOffset.left + 'px',
-          width: this.$el.offsetWidth + 'px'
-        }
-      }
-      if (this.affixed && scrollTop < elementOffset.top) {
-        this.affixed = false
-        this.styles = {}
-      }
-    },
     // from https://github.com/ant-design/ant-design/blob/master/components/affix/index.jsx#L20
-    getScroll (w, top) {
-      let ret = w['page' + (top ? 'Y' : 'X') + 'Offset']
-      const method = 'scroll' + (top ? 'Top' : 'Left')
-      if (typeof ret !== 'number') {
-        const d = w.document
-        // ie6,7,8 standard mode
-        ret = d.documentElement[method]
-        if (typeof ret !== 'number') {
-          // quirks mode
-          ret = d.body[method]
-        }
-      }
-      return ret
-    },
-    getOffset (element) {
-      const rect = element.getBoundingClientRect()
+    checkScroll () {
+      // if is hidden don't calculate anything
+      if (!(this.$el.offsetWidth || this.$el.offsetHeight || this.$el.getClientRects().length)) { return }
+      // get window scroll and element position to detect if have to be normal or affixed
+      let scroll = {}
+      let element = {}
+      const rect = this.$el.getBoundingClientRect()
       const body = document.body
-      const clientTop = element.clientTop || body.clientTop || 0
-      const clientLeft = element.clientLeft || body.clientLeft || 0
-      const scrollTop = this.getScroll(window, true)
-      const scrollLeft = this.getScroll(window)
-      return {
-        top: rect.top + scrollTop - clientTop,
-        left: rect.left + scrollLeft - clientLeft
+      for(let type of ['Top','Left']) {
+        let t = type.toLowerCase()
+        let ret = window['page' + (type==='Top' ? 'Y' : 'X') + 'Offset']
+        const method = 'scroll' + type
+        if (typeof ret !== 'number') {
+          // ie6,7,8 standard mode
+          ret = document.documentElement[method]
+          if (typeof ret !== 'number') {
+            // quirks mode
+            ret = document.body[method]
+          }
+        }
+        scroll[t] = ret
+        element[t] = scroll[t] + rect[t] - (this.$el['client'+type] || body['client'+type] || 0)
       }
+      let fix = scroll.top > element.top - this.offset
+      if (this.affixed !== fix) { this.affixed = fix }
     }
   },
   ready () {
-    $(window).on('scroll resize', () => this.scrolling())
+    this.checkScroll()
+    $(window).on('scroll resize', () => this.checkScroll())
   },
   beforeDestroy () {
-    $(window).off('scroll resize', () => this.scrolling())
+    $(window).off('scroll resize', () => this.checkScroll())
   }
 }
 </script>
-
-<style>
-  .vue-affix {
-    position: fixed;
-  }
-</style>
