ea54f4d3d89f39b3493f169f562a5e7c98da7194
mqli <limanqing315@gmail.com>
2016-08-22 14:33:52 +0800

    update 'translations.js ' for zh-CN support

--- src/translations.js
+++ src/translations.js
@@ -1,20 +1,22 @@
-// return all the translations
-let text = {
   daysOfWeek: {
     en: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-    es: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa']
+    es: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+    'zh-CN':['日','一', '二', '三', '四', '五', '六']
   },
   limit: {
     en: 'Limit reached ({{limit}} items max).',
-    es: 'Limite alcanzado (máximo {{limit}} items).'
+    es: 'Limite alcanzado (máximo {{limit}} items).',
+    'zh-CN': '达到最大限制（{{limit}}）'
   },
   loading: {
     en: 'Loading...',
-    es: 'Cargando...'
+    es: 'Cargando...',
+    'zh-CN': '加载中...'
   },
   minLength: {
     en: 'Min. Length',
-    es: 'Tamaño Mínimo'
+    es: 'Tamaño Mínimo',
+    'zh-CN': '最小长度'
   },
   months: {
     en: [
@@ -28,19 +30,27 @@ let text = {
       'Abril', 'Mayo', 'Junio',
       'Julio', 'Agosto', 'Septiembre',
       'Octubre', 'Noviembre', 'Diciembre'
-    ]
+    ],
+    'zh-CN':[
+      '一月','二月', '三月',
+      '四月', '五月', '六月',
+      '七月','八月', '九月',
+      '十月', '十一月', '十二月']
   },
   notSelected: {
     en: 'Nothing Selected',
-    es: 'Nada seleccionado'
+    es: 'Nada seleccionado',
+    'zh-CN': '未选中任何选项',
   },
   required: {
     en: 'Required',
-    es: 'Requerido'
+    es: 'Requerido',
+    'zh-CN': '必填',
   },
   search: {
     en: 'Search',
-    es: 'Buscar'
+    es: 'Buscar',
+    'zh-CN': '查找',
   }
 }
 export default (lang = 'en') => {
