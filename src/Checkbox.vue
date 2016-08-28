fee0f5fc645a76c2af04db575d18dfe4a2d75460
Willem <willemfranco@gmail.com>
2016-08-28 16:50:40 -0400

    radio and checkbox support theming
    - replaced all the personalized colors
    - colors based on .btn colors, matching that way to the current theme.

--- src/Checkbox.vue
+++ src/Checkbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <label v-if="buttonStyle" :class="['btn btn-'+typeColor,{active:checked,disabled:disabled,readonly:readonly}]" @click="toggle">
+  <label v-if="buttonStyle" :class="['btn btn-'+typeColor,{active:checked,disabled:disabled,readonly:readonly}]" @click.prevent="toggle">
     <input type="checkbox" autocomplete="off"
       v-el:input
       v-show="!readonly"
@@ -11,8 +11,8 @@
     />
     <slot></slot>
   </label>
-  <div v-else :class="['checkbox',typeColor,{active:checked,disabled:disabled,readonly:readonly}]" @click="toggle">
-    <label>
+  <div v-else :class="['checkbox',typeColor,{active:checked,disabled:disabled,readonly:readonly}]" @click.prevent="toggle">
+    <label class="open">
       <input type="checkbox" autocomplete="off"
         v-el:input
         :checked="active"
@@ -21,7 +21,8 @@
         :readonly="readonly"
         :disabled="disabled"
       />
-      <span class="icon"></span>
+      <span class="icon dropdown-toggle" :class="[active?'btn-'+typeColor:'',{bg:typeColor==='default'}]"></span>
+      <span v-if="active&&typeColor==='default'" class="icon"></span>
       <slot></slot>
     </label>
   </div>
@@ -96,16 +97,14 @@ export default {
     focus () {
       this.$els.input.focus()
     },
-    toggle (e) {
-      e.preventDefault();
+    toggle () {
       if (!this.disabled) {
         this.focus()
         if (!this.readonly) {
           this.checked = this.checked ? null : this.value
           if (this.group && typeof this.value !== 'boolean') {
-            const parent = this.$parent
-            const index = parent.value.indexOf(this.value)
-            parent.value[~index ? '$remove' : 'push'](this.value)
+            const index = this.$parent.value.indexOf(this.value)
+            this.$parent.value[~index ? '$remove' : 'push'](this.value)
           }
         }
       }
@@ -118,12 +117,12 @@ export default {
 <style scoped>
 .checkbox { position: relative; }
 .checkbox > label > input {
+  box-sizing: border-box;
   position: absolute;
-  margin: 0;
+  z-index: -1;
   padding: 0;
   opacity: 0;
-  z-index: -1;
-  box-sizing: border-box;
+  margin: 0;
 }
 .checkbox > label > .icon {
   position: absolute;
@@ -134,38 +133,31 @@ export default {
   height: 1.4rem;
   line-height:1rem;
   text-align: center;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
   user-select: none;
   border-radius: .35rem;
-  background-color: #ddd;
   background-repeat: no-repeat;
   background-position: center center;
   background-size: 50% 50%;
 }
+.checkbox:not(.active) > label > .icon {
+  background-color: #ddd;
+  border: 1px solid #bbb;
+}
 .checkbox > label > input:focus ~ .icon {
   outline: 0;
   border: 1px solid #66afe9;
-  -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
   box-shadow: inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);
 }
 .checkbox.active > label > .icon {
   background-size: 1rem 1rem;
-  background-image: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNy4xLjAsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB2aWV3Qm94PSIwIDAgOCA4IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDAgMCA4IDgiIHhtbDpzcGFjZT0icHJlc2VydmUiPg0KPHBhdGggZmlsbD0iI0ZGRkZGRiIgZD0iTTYuNCwxTDUuNywxLjdMMi45LDQuNUwyLjEsMy43TDEuNCwzTDAsNC40bDAuNywwLjdsMS41LDEuNWwwLjcsMC43bDAuNy0wLjdsMy41LTMuNWwwLjctMC43TDYuNCwxTDYuNCwxeiINCgkvPg0KPC9zdmc+DQo=);
+  background-image: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHZpZXdCb3g9IjAgMCA4IDgiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDggOCIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHBhdGggZmlsbD0iI2ZmZiIgZD0iTTYuNCwxTDUuNywxLjdMMi45LDQuNUwyLjEsMy43TDEuNCwzTDAsNC40bDAuNywwLjdsMS41LDEuNWwwLjcsMC43bDAuNy0wLjdsMy41LTMuNWwwLjctMC43TDYuNCwxTDYuNCwxeiIvPjwvc3ZnPg0K);
 }
-.checkbox.active.default > label > .icon { background-color: #bbb; }
-.checkbox.active.primary > label > .icon { background-color: #337ab7; }
-.checkbox.active.success > label > .icon { background-color: #5cb85c; }
-.checkbox.active.info > label > .icon { background-color: #5bc0de; }
-.checkbox.active.warning > label > .icon { background-color: #f0ad4e; }
-.checkbox.active.danger > label > .icon { background-color: #d9534f; }
+.checkbox.active .btn-default { filter: brightness(75%); }
 
 .checkbox.disabled > label > .icon,
 .checkbox.readonly > label > .icon,
 .btn.readonly {
   filter: alpha(opacity=65);
-  -webkit-box-shadow: none;
   box-shadow: none;
   opacity: .65;
 }
