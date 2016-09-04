c98563d66e36bfcd94bb5f0a7623aa38dd773b2c
Willem <willemfranco@gmail.com>
2016-09-04 18:08:20 -0400

    separated language translations
    - in order to mantain a cleaner source code, the languages now was
    separated from the main js, so now you have to include the language file
    manualy.

--- src/translations.js
+++ src/translations.js
@@ -1,78 +1,14 @@
 // return all the translations
 let text = {
-  daysOfWeek: {
-    en: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-    es: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
-    'pt-BR': ['Do', 'Se', 'Te', 'Qa', 'Qi', 'Sx', 'Sa'],
-    fr: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa']
-  },
-  limit: {
-    en: 'Limit reached ({{limit}} items max).',
-    es: 'Limite alcanzado (máximo {{limit}} items).',
-    'pt-BR': 'Limite atingido (máximo {{limit}} items).',
-    fr: 'Limite atteinte ({{limit}} éléments max).'
-  },
-  loading: {
-    en: 'Loading...',
-    es: 'Cargando...',
-    'pt-BR': 'Cargando...',
-    fr: 'Chargement en cours...'
-  },
-  minLength: {
-    en: 'Min. Length',
-    es: 'Tamaño Mínimo',
-    'pt-BR': 'Tamanho Mínimo',
-    fr: 'Longueur Minimum'
-  },
-  months: {
-    en: [
-      'January', 'February', 'March',
-      'April', 'May', 'June',
-      'July', 'August', 'September',
-      'October', 'November', 'December'
-    ],
-    es: [
-      'Enero', 'Febrero', 'Marzo',
-      'Abril', 'Mayo', 'Junio',
-      'Julio', 'Agosto', 'Septiembre',
-      'Octubre', 'Noviembre', 'Diciembre'
-    ],
-    'pt-BR': [
-      'Janeiro', 'Fevereiro', 'Março',
-      'Abril', 'Maio', 'Junho',
-      'Julho', 'Agosto', 'Setembro',
-      'Outubro', 'Novembro', 'Dezembro'
-    ],
-    fr: [
-      'Janvier', 'Février', 'Mars',
-      'Avril', 'Mai', 'Juin',
-      'Juillet', 'Août', 'Septembre',
-      'Octobre', 'Novembre', 'Décembre'
-    ]
-  },
-  notSelected: {
-    en: 'Nothing Selected',
-    es: 'Nada seleccionado',
-    'pt-BR': 'Nada selecionado',
-    fr: 'Aucune sélection'
-  },
-  required: {
-    en: 'Required',
-    es: 'Requerido',
-    'pt-BR': 'Requerido',
-    fr: 'Requis'
-  },
-  search: {
-    en: 'Search',
-    es: 'Buscar',
-    'pt-BR': 'Buscar',
-    fr: 'Recherche'
-  }
+  daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+  limit: 'Limit reached ({{limit}} items max).',
+  loading: 'Loading...',
+  minLength: 'Min. Length',
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  notSelected: 'Nothing Selected',
+  required: 'Required',
+  search: 'Search'
 }
 export default (lang = 'en') => {
-  let tr = {}
-  for (let i in text) {
-    tr[i] = text[i][lang] || text[i]['en']
-  }
-  return tr
+  return window.VueStrapLang ? window.VueStrapLang(lang) : text
 }
