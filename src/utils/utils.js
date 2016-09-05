bde54dce9f29a7e9b9cdd3e8f7eaba6c65b7117a
Willem Franco <willemfranco@gmail.com>
2016-09-05 16:36:56 -0400

    correcting translations issues
    - added translator into utils.js pack

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -77,3 +77,19 @@ export function getScrollBarWidth () {
 
   return (w1 - w2)
 }
+
+// return all the translations or the default language (english)
+export function translations (lang) {
+  lang = lang || 'en'
+  let text = {
+    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    limit: 'Limit reached ({{limit}} items max).',
+    loading: 'Loading...',
+    minLength: 'Min. Length',
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    notSelected: 'Nothing Selected',
+    required: 'Required',
+    search: 'Search'
+  }
+  return window.VueStrapLang ? window.VueStrapLang(lang) : text
+}
