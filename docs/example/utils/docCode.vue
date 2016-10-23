c29533e8aae52c8ffab0cf288d062e8918620c47
Willem <willemfranco@gmail.com>
2016-10-23 16:20:12 -0400

    docCode disabled

--- docs/example/utils/docCode.vue
+++ docs/example/utils/docCode.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <pre v-if="!language" ref="container"></pre>
+    docCode disabled (example codes)
+    <!-- <pre v-if="!language" ref="container"></pre>
     <template v-else>
-      <pre v-if="language=='markup'"><code ref="container" class="language-markup"><slot></slot><!-- <script type="language-mark-up"></script> --></code></pre>
-      <pre v-else><code ref="container" :class="'language-' + language"><slot></slot><!-- <script :type="'language-' + language"></script> --></code></pre>
-    </template>
+      <pre v-if="language=='markup'"><code ref="container" class="language-markup"><slot></slot><script type="language-mark-up"></script></code></pre>
+      <pre v-else><code ref="container" :class="'language-' + language"><slot></slot><script :type="'language-' + language"></script></code></pre>
+    </template> -->
   </div>
 </template>
 
 <script>
-import $ from 'src/utils/NodeList.js'
+import $ from 'src/components/utils/NodeList.js'
 
 export default {
   props: {
