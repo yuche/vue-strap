6a6aae8a7db9ee06edb034a23ad7d8adb2169814
Willem Franco <willemfranco@gmail.com>
2016-11-03 16:44:08 -0400

    delayer utility
    - create a function that execute after a delay, if you call it again
    before the time finish, the previous call is canceled. work with a
    static value or with a variable in the data / props

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -94,6 +94,18 @@ export function translations (lang = 'en') {
   return window.VueStrapLang ? window.VueStrapLang(lang) : text
 }
 
+// delayer: set a function that execute after a delay. Work with a prop timer set in the vue object, or a direct number value.
+export function delayer (fn, varTimer, ifNaN = 100) {
+  function toInt (el) { return /^[0-9]+$/.test(el) ? Number(el) || 1 : null }
+  var timerId
+  return function (...args) {
+    if (timerId) clearTimeout(timerId)
+    timerId = setTimeout(() => {
+      fn.apply(this, args)
+    }, toInt(varTimer) || toInt(this[varTimer]) || ifNaN)
+  }
+}
+
 var blurList = []
 export function oneBlur (node, callback) {
   if (!callback) { return }
