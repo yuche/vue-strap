0eb670dd6329242de1cd2e90fb7b3b3697b0dc13
Willem <willemfranco@gmail.com>
2016-11-12 14:25:28 -0400

    Fixing some utils.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 // coerce convert som types of data into another type
 export const coerce = {
   // Convert a string to booleam. Otherwise, return the value without modification, so if is not boolean, Vue throw a warning.
-  boolean: val => (typeof val === 'string' ? (val === 'false' || val === 'null' || val === 'undefined' ? false : val === 'true' ? true : val) : val),
+  boolean: val => (typeof val === 'string' ? val === '' || val === 'true' ? true : (val === 'false' || val === 'null' || val === 'undefined' ? false : val) : val),
   // Attempt to convert a string value to a Number. Otherwise, return 0.
   number: (val, alt = null) => (typeof val === 'number' ? val : val === undefined || val === null || isNaN(Number(val)) ? alt : Number(val)),
   // Attempt to convert to string any value, except for null or undefined.
@@ -11,10 +11,10 @@ export const coerce = {
 }
 
 export function getJSON (url) {
-  let request = new window.XMLHttpRequest()
-  let data = {}
+  var request = new window.XMLHttpRequest()
+  var data = {}
   // p (-simulated- promise)
-  let p = {
+  var p = {
     then (fn1, fn2) { return p.done(fn1).fail(fn2) },
     catch (fn) { return p.fail(fn) },
     always (fn) { return p.done(fn).fail(fn) }
@@ -32,15 +32,16 @@ export function getJSON (url) {
       let e = {status: request.status}
       if (request.status === 200) {
         try {
-          let value, response = request.responseText
-          data.done.forEach(done => {
-            if ((value = done(response)) !== undefined) { response = value }
-          })
-        } catch (e) {
-          data.fail.forEach(fail => { fail(e) })
+          var response = request.responseText
+          for (var i in data.done) {
+            var value = data.done[i](response)
+            if (value !== undefined) { response = value }
+          }
+        } catch (err) {
+          data.fail.forEach(fail => fail(err))
         }
       } else {
-        data.fail.forEach(fail => { fail(e) })
+        data.fail.forEach(fail => fail(e))
       }
     }
   }
@@ -80,8 +81,7 @@ export function getScrollBarWidth () {
 }
 
 // return all the translations or the default language (english)
-export function translations (lang) {
-  lang = lang || 'en'
+export function translations (lang = 'en') {
   let text = {
     daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
     limit: 'Limit reached ({{limit}} items max).',
@@ -93,4 +93,57 @@ export function translations (lang) {
     search: 'Search'
   }
   return window.VueStrapLang ? window.VueStrapLang(lang) : text
+}
+
+// delayer: set a function that execute after a delay
+// @params (function, delay_prop or value, default_value)
+export function delayer (fn, varTimer, ifNaN = 100) {
+  function toInt (el) { return /^[0-9]+$/.test(el) ? Number(el) || 1 : null }
+  var timerId
+  return function (...args) {
+    if (timerId) clearTimeout(timerId)
+    timerId = setTimeout(() => {
+      fn.apply(this, args)
+    }, toInt(varTimer) || toInt(this[varTimer]) || ifNaN)
+  }
+}
+
+// Fix a vue instance Lifecycle to vue 1/2 (just the basic elements, is not a real parser, so this work only if your code is compatible with both)
+export function VueFixer (vue) {
+  var vue2 = !window.Vue || !window.Vue.partial
+  var mixin = {
+    computed: {
+      vue2 () { return !this.$dispatch }
+    }
+  }
+  if (!vue2) {
+    if (vue.beforeCreate) {
+      mixin.create = vue.beforeCreate
+      delete vue.beforeCreate
+    }
+    if (vue.beforeMount) {
+      vue.beforeCompile = vue.beforeMount
+      delete vue.beforeMount
+    }
+    if (vue.mounted) {
+      vue.ready = vue.mounted
+      delete vue.mounted
+    }
+  } else {
+    if (vue.beforeCompile) {
+      vue.beforeMount = vue.beforeCompile
+      delete vue.beforeCompile
+    }
+    if (vue.compiled) {
+      mixin.compiled = vue.compiled
+      delete vue.compiled
+    }
+    if (vue.ready) {
+      vue.mounted = vue.ready
+      delete vue.ready
+    }
+  }
+  if (!vue.mixins) { vue.mixins = [] }
+  vue.mixins.unshift(mixin)
+  return vue
 }
