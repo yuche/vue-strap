134feb0578ce2d755ed032c4bd42168b0c843148
Willem <willemfranco@gmail.com>
2016-11-27 21:40:29 -0400

    fixed spinner

--- src/Spinner.vue
+++ src/Spinner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['spinner spinner-gritcode',spinnerSize,{'spinner-fixed':fixed}]" v-show="active">
+  <div :class="['spinner spinner-gritcode',spinnerSize,{'spinner-fixed':fixed}]" v-show="active||locked">
     <div class="spinner-wrapper">
       <div class="spinner-circle"></div>
       <div class="spinner-text">{{text}}</div>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import {coerce, delayer} from './utils/utils.js'
 const MIN_WAIT = 500 // in ms
 
 export default {
@@ -15,66 +16,71 @@ export default {
     fixed: {type: Boolean, default: false},
     global: {type: Boolean, default: false},
     size: {type: String, default: 'md'},
-    text: {type: String, default: ''}
+    text: {type: String, default: ''},
+    value: {default: false}
   },
   data () {
     return {
-      active: false
+      active: this.value,
+      locked: false
     }
   },
   computed: {
-    spinnerSize () { return this.size ? 'spinner-' + this.size : 'spinner-sm' }
+    spinnerSize () { return 'spinner-' + (this.size ? this.size : 'sm') }
+  },
+  watch: {
+    active (val, old) {
+      if (val !== old) this.$emit('input', val)
+    },
+    value (val, old) {
+      if (val !== old) { this[val ? 'show' : 'hide']() }
+    }
   },
   methods: {
-    getMinWait (delay) {
-      delay = delay || 0
-      return new Date().getTime() - this._started.getTime() < MIN_WAIT ? MIN_WAIT - parseInt(new Date().getTime() - this._started.getTime(), 10) + delay : 0 + delay
+    hide () {
+      var delay = 0
+      this.active = false
     },
     show (options) {
-      if (options && options.text) {
-        this.text = options.text
+      if (options) {
+        if (options.text) { this.text = options.text }
+        if (options.size) { this.size = options.size }
+        if (options.fixed) { this.fixed = options.fixed }
       }
-      if (options && options.size) {
-        this.size = options.size
-      }
-      if (options && options.fixed) {
-        this.fixed = options.fixed
-      }
-
       // block scrolling when spinner is on
       this._body.style.overflowY = 'hidden'
-
       // activate spinner
       this._started = new Date()
       this.active = true
-      this.$root.$emit('shown::spinner')
-    },
-    hide () {
-      const delay = 0
-      this._spinnerAnimation = setTimeout(() => {
-        this.active = false
-        this._body.style.overflowY = this._bodyOverflow
-        this.$root.$emit('hidden::spinner')
-      }, this.getMinWait(delay))
+      this.locked = true
+      this._unlock()
     }
   },
-  events: {
-    'show::spinner' (options) { this.show(options) },
-    'hide::spinner' () { this.hide() },
-    'start::ajax' (options) { this.show(options) },
-    'end::ajax' () { this.hide() }
-  },
-  mounted () {
+  created () {
+    this._body = document.body
+    this._bodyOverflow = document.body.style.overflowY
+    this._unlock = delayer(function () {
+      this.locked = false
+      this._body.style.overflowY = this._bodyOverflow
+    }, MIN_WAIT)
     if (this.global) {
-      this._global = this.global
-      this.$root.$on('spinner::show', () => this.show)
-      this.$root.$on('spinner::hide', () => this.hide)
+      if (!this.$root._globalSpinner) {
+        this.$root._globalSpinner = true
+        var self = this
+        this._global = {
+          hide () { self.hide() },
+          show () { self.show() }
+        }
+        this.$root.$on('spinner::show', this._global.show)
+        this.$root.$on('spinner::hide', this._global.hide)
+      }
     }
   },
   beforeDestroy () {
     if (this._global) {
-      this.$root.$off('spinner::show', () => this.show)
-      this.$root.$off('spinner::hide', () => this.hide)
+      this.$root.$off('spinner::show', this._global.show)
+      this.$root.$off('spinner::hide', this._global.hide)
+      delete this.$root._globalSpinner
     }
     clearTimeout(this._spinnerAnimation)
     this._body.style.overflowY = this._bodyOverflow
