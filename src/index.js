3f4c4ad5a7359a50597e4db7d80a868a009a7b27
Yarob Al-Taay <yarob83@gmail.com>
2017-04-28 19:57:54 +0100

    remove debugging codes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
-// import $ from './utils/NodeList.js'
-// import accordion from './Accordion.vue'
-// import affix from './Affix.vue'
-// import alert from './Alert.vue'
-// import aside from './Aside.vue'
-// import buttonGroup from './buttonGroup.vue'
-// import carousel from './Carousel.vue'
-// import checkbox from './Checkbox.vue'
-// import datepicker from './Datepicker.vue'
-// import dropdown from './Dropdown.vue'
-// import formGroup from './FormGroup.vue'
-// import input from './Input.vue'
-// import modal from './Modal.vue'
-// import navbar from './Navbar.vue'
-// import option from './Option.vue'
-// import panel from './Panel.vue'
-// import popover from './Popover.vue'
-// import progressbar from './Progressbar.vue'
-// import radio from './Radio.vue'
-// import select from './Select.vue'
-// import slider from './Slider.vue'
-// import spinner from './Spinner.vue'
-// import tab from './Tab.vue'
-// import tabGroup from './TabGroup.vue'
-// import tabset from './Tabset.vue'
-// import tooltip from './Tooltip.vue'
-// import typeahead from './Typeahead.vue'
-// export default modal = require('./Modal.vue');
-
-
-module.exports = {z: 'z value'};
-
 module.exports = {
-  // $,
+  $: require('./utils/NodeList.js'),
   accordion: require('./Accordion.vue'),
   affix: require('./Affix.vue'),
   alert: require('./Alert.vue'),
